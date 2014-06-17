567b468f49cace111366980541c7187bfa224e5e
Arnost Valicek <arnost.valicek@gmail.com>
2014-06-17 21:35:57 +0200

    test rendering multiple modules

--- src/test/scala/xquery_source_code_generator/ModuleTest.scala
+++ src/test/scala/xquery_source_code_generator/ModuleTest.scala
@@ -4,17 +4,38 @@ import org.specs2.mutable.Specification
 
 
 class ModuleTest extends Specification {
-  "Module" should {
+  "Single module" should {
     "contain one empty function" in {
        val function = FunctionBody("functOne")
        val module = Module(NsPrefix("nsone","moduleA"), List(function))
 
-       module.renderedSource === """module namespace moduleA = "nsone";
-                                   |
-                                   |
-                                   |declare function functOne() {
-                                   |  ()
-                                   |};""".stripMargin
+       module.renderedSource ===
+         """module namespace moduleA = "nsone";
+           |
+           |
+           |declare function functOne() {
+           |  ()
+           |};""".stripMargin
+    }
+
+    "contain multiple functions" in {
+      val fun1 = FunctionBody("funOne")
+      val fun2 = FunctionBody("funTwo")
+      val ns = NsPrefix("ns-one", "moduleA")
+
+      val module = Module(ns, List(fun1, fun2))
+
+      module.renderedSource ===
+        """module namespace moduleA = "ns-one";
+          |
+          |
+          |declare function funOne() {
+          |  ()
+          |};
+          |
+          |declare function funTwo() {
+          |  ()
+          |};""".stripMargin
     }
   }
 }
