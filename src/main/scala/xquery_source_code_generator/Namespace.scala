32fa15189c629ac7bba525e1d88d849cee7f8dd4
Arnost Valicek <arnost.valicek@gmail.com>
2014-06-25 21:31:20 +0200

    namespace files in one file

--- src/main/scala/xquery_source_code_generator/Namespace.scala
+++ src/main/scala/xquery_source_code_generator/Namespace.scala
@@ -1,3 +1,5 @@
 package xquery_source_code_generator
 
 case class Namespace(uri: String)
+
+case class NsPrefix(namespace: Namespace, prefix: String)
--- src/main/scala/xquery_source_code_generator/NsPrefix.scala
+++
@@ -1,4 +0,0 @@
-package xquery_source_code_generator
-
-
-case class NsPrefix(namespace: Namespace, prefix: String)
