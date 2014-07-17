68a39f9dc70db41d0fef9136412d16b5f2229581
Arnost Valicek <arnost.valicek@gmail.com>
2014-07-17 08:36:11 +0200

    build module with few functions there

--- src/main/scala/xquery_source_code_generator/ModuleBuilder.scala
+++ src/main/scala/xquery_source_code_generator/ModuleBuilder.scala
@@ -2,8 +2,9 @@ package xquery_source_code_generator
 
 class ModuleBuilder(modules:ModuleCollection) {
 
-  def buildModule(nsPrefix: NsPrefix): Module = {
-    Module(nsPrefix, List())
+  def buildModule(nsPrefix: NsPrefix, numberOfFunctions: Int): Module = {
+    val functions = for (i: Int <- 1 to numberOfFunctions) yield FunctionBody(s"func$i", List())
+    Module(nsPrefix, functions.toList)
   }
 
 }
--- src/test/scala/xquery_source_code_generator/ModuleBuilderTest.scala
+++ src/test/scala/xquery_source_code_generator/ModuleBuilderTest.scala
@@ -5,12 +5,21 @@ import org.specs2.mutable.Specification
 
 class ModuleBuilderTest extends Specification {
   "module builder" should {
-    "build first module when module collection is empty" in {
+    "build empty module when module collection is empty" in {
       val modules = new ModuleCollection
       val builder = new ModuleBuilder(modules)
 
-      val module = builder.buildModule(NsPrefix(Namespace("my://namespace/one"), "ns1"))
+      val module = builder.buildModule(NsPrefix(Namespace("my://namespace/one"), "ns1"), 0)
       module === Module(NsPrefix(Namespace("my://namespace/one"), "ns1"), List())
+    }
+
+    "build module with few functions" in {
+      val modules = new ModuleCollection
+      val builder = new ModuleBuilder(modules)
+
+      val module = builder.buildModule(NsPrefix(Namespace("my://namespace/one"), "ns1"), 3)
+      module.functions.size === 3
+
     }
   }
 }
