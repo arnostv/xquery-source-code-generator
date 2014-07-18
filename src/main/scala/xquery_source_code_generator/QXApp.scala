977432610b89c2f870c90fd58cfc14f04ec8ba36
Arnost Valicek <arnost.valicek@gmail.com>
2014-07-18 20:16:03 +0200

    generated .xq files are written to local directory "generator-output"

--- src/main/scala/xquery_source_code_generator/QXApp.scala
+++ src/main/scala/xquery_source_code_generator/QXApp.scala
@@ -1,6 +1,24 @@
 package xquery_source_code_generator
 
+import java.io.{File, FileWriter}
+
 object QXApp {
+
+  val generatorRootDirectory = cleanGeneratedOutputDir()
+
+  def cleanGeneratedOutputDir() = {
+    val dir = new File("generator-output")
+    if (!dir.isDirectory) {
+      throw new RuntimeException("Directory " + dir.getCanonicalPath + " does not exist")
+    }
+
+    for (file <- dir.listFiles()) {
+      file.delete()
+    }
+
+    dir
+  }
+
 
   def main(args: Array[String]) = {
     val collection = new ModuleCollection
@@ -9,8 +27,21 @@ object QXApp {
     val modules: Seq[Module] = builder.buildBunchOfModules(4)
 
     for (module <- modules) {
-      println(module.renderedSource)
+      println(module.nsPrefix.namespace)
+      val toFile: File = nsPrefixToFile(module.nsPrefix)
+      println(toFile)
+
+      val writer = new FileWriter(toFile)
+      writer.write(module.renderedSource)
+      writer.close()
     }
+
+    def nsPrefixToFile(nsPrefix: NsPrefix): File = {
+      val last = nsPrefix.namespace.uri.split("/").last
+
+      new File(generatorRootDirectory, last + ".xq")
+    }
+
   }
 
 }
